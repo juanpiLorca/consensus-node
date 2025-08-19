dfeb16709be7b88fe784888360b704879332cc2d
Juan Ignacio <juan.lorca@uc.cl>
2025-08-19 17:58:07 -0400

    fixed raspberry/edge.js

--- raspberry/edge.js
+++ raspberry/edge.js
@@ -42,10 +42,10 @@ if (TYPE == TYPE_BLE) {
         // (1) Network params: { enabled, node, neighbors } --> 'n'
         // (2) Consensus params: { clock, state, vstate, vartheta, eta, disturbance.random, disturbance.offset, disturbance.amplitude, disturbance.phase, disturbance.samples } --> 'a'
         // (3) Trigger params: { trigger } --> 't'
-        const msgNetwork = `n,${params.enabled ? 1 : 0},${params.node},${params.neighbors.join(',')}\n\r`;
-        const msgConsensus = `a,${params.clock},${params.state},${params.vstate},${params.vartheta},${params.eta},${params.disturbance.random ? 1 : 0},` + 
+        const msgNetwork = `n${params.enabled ? 1 : 0},${params.node},${params.neighbors.join(',')}\n\r`;
+        const msgConsensus = `a${params.clock},${params.state},${params.vstate},${params.vartheta},${params.eta},${params.disturbance.random ? 1 : 0},` + 
             `${params.disturbance.offset},${params.disturbance.amplitude},${params.disturbance.phase},${params.disturbance.samples}\n\r`;
-        const msgTrigger = `t,${params.trigger ? 1 : 0}\n\r`;
+        const msgTrigger = `t${params.trigger ? 1 : 0}\n\r`;
 
         try {
             await serialWrite(msgNetwork); 
